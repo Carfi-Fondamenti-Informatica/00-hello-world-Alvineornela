f349515999834712830de511acc9ed843a81e591
Alvineornela <127629007+Alvineornela@users.noreply.github.com>
2023-03-21 22:52:20 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,5 +2,14 @@
 using namespace std;
 
 int main() {
-   return 0;
+    int dividendo=431;
+    int divisore=17;
+    int quoziente=dividendo/divisore;
+    int resto=dividendo%divisore;
+    std::cout<<dividendo<<std::endl;
+    std::cout<<divisore<<std::endl;
+    std::cout<<quoziente<< std::endl;
+    std::cout<<resto<< std::endl;
+
+    return 0;
 }
